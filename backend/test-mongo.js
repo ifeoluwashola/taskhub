6e4aa7f3e3f5ffbff2ac5261fc0b1182807aae6e
Oloniyo-Bolaji <ayoolabolaji12@yahoo.com>
2026-07-18 05:51:03 +0100

    Add .dockerignore file to backend directory and extract environmental variables

--- backend/test-mongo.js
+++ backend/test-mongo.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 async function run() {
-  const client = new MongoClient('mongodb://localhost:27017');
+  const mongoClient = process.env.MONGODB_CLIENT;
+  const mongodbInitdbDatabase = process.env.MONGO_INITDB_DATABASE;
+  
+  const client = new MongoClient(mongoClient);
   await client.connect();
-  const db = client.db('kybern-taskhub');
+  const db = client.db(mongodbInitdbDatabase);
   const tasks = await db.collection('tasks').find().toArray();
   console.log(JSON.stringify(tasks, null, 2));
   await client.close();
